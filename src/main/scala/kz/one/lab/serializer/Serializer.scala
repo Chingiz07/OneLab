4cb6fbd0a1ba14aa0ed531263159b012ccad9533
SHSalambaev <shsalambaev@gmail.com>
2021-06-15 17:26:52 +0600

    template

--- src/main/scala/kz/one/lab/serializer/Serializer.scala
+++ src/main/scala/kz/one/lab/serializer/Serializer.scala
@@ -1,16 +1,14 @@
 package kz.one.lab.serializer
 
-import org.json4s.{Formats, ShortTypeHints, jackson}
+import org.json4s.{Formats, ShortTypeHints}
 import org.json4s.native.Serialization
 
-trait Serializer extends CoreSerializer {
+trait Serializer extends CoreSerializer with Serialization {
 
   implicit val formats: Formats = Serialization.formats(
     ShortTypeHints(
       hints
     )
   )
-
-  implicit val serialization: jackson.Serialization.type = jackson.Serialization
 
 }
